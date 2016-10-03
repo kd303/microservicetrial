e8759f24de74d9d2abd4ee4e2b1eb86ce0cb082d
kd303 <kaushalmdesai@gmail.com>
2016-10-03 15:32:30 +0530

    initial commit

---
+++ configserver/src/main/java/org/vehnicaltrackking/configserver/ConfigServer.java
@@ -0,0 +1,14 @@
+package org.vehnicaltrackking.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServer {
+
+	public static void main(String []s) {
+		SpringApplication.run(ConfigServer.class, s);
+	}
+}
